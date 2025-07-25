c17b1eb8c5cda4ac2a18981388c5e2a219b94441
Anndre595 <andre.monteiro0914@gmail.com>
2025-07-24 21:27:00 -0300

    Aula 04

---
+++ Aula 04/script.js
@@ -0,0 +1,51 @@
+//Excercicio 01 - imprimir o numero de 0 a 10 no console 
+/*
+
+for (let i = 1   ;i<20    ; i++   ){
+console.log (i)
+alert(i) 
+
+
+
+
+//Excercicio 02 - imprimir os numero pares de 0 a 20 no console 
+/*
+
+for (let i = 1   ;i<20    ; i++   ){
+    if (i % 2 === 0) {
+console.log (i)
+alert(i) 
+    
+} 
+*/ //Excercicio 03 Somar 0s numero de 1 a 100
+
+/*
+for (let i = 1   ;i=<100    ; i++   ){
+    if (i = 2 === 0) {
+console.log (i)
+alert(i)
+
+   /Excercicio 04 
+}  */
+/*
+for (let i = 0   ;i<=10    ; i++   ){
+    let resultado = i * 5
+console.log (resultado)
+alert(resultado) 
+    
+
+}*/
+
+
+//Excercicio 05
+/*
+let numerotb = Number (prompt ('Digite o numero da tabuada'))
+for (let i = 0   ;i<=10    ; i++   ){
+    let resultado = i * numerotb
+console.log (resultado)
+alert(resultado) 
+
+} */
+
+
+
